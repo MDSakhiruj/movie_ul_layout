060542bad74887be0b8a31b967553904e1868060
MDSakhiruj <sakhiruj.jaman.salim@gmail.com>
2022-10-05 16:51:47 +0800

    api linnk

---
+++ script.js
@@ -0,0 +1,3 @@
+const apiURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2aaf606463d9390bf6ceee2e230972ad&page=1';
+const imgPath = 'https://image.tmdb.org/t/p/w1280';
+const searchURL =   
